252faaa2e02b4bb4609a1d364e67667719479dea
IagoLucioGG <iagolucioiagi@gmail.com>
2024-03-17 01:41:34 -0300

    feat: Implementação de Token de acesso a API.

---
+++ src/Components/LoginForm/index.jsx
@@ -0,0 +1,54 @@
+'use client'
+import React, { useState, useEffect } from 'react';
+
+const loginForm = () => {
+    const loginInfo = {
+        nonce: '123456789',
+        username: 'BimerAPI',
+        client_id: 'BimerAPI',
+        senha: '123456',
+        password: 'fe55bd6e22edd2889c2ce97cf6215928',
+        grant_type: 'password'
+    };
+
+    const apiUrl = 'http://localhost:8091';
+
+    const [tokenInfo, setTokenInfo] = useState(null);
+
+    const login = async () => {
+        try {
+            const response = await fetch(`${apiUrl}/oauth/token`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: new URLSearchParams(loginInfo).toString()
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const responseData = await response.json();
+            setTokenInfo(responseData.access_token);
+
+            console.log('Login Efetuado com Sucesso!');
+            
+        } catch (error) {
+            console.error('Erro ao fazer login:', error.message);
+        }
+    };
+
+    
+
+    useEffect(() => {
+        login(); 
+    }, []); 
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default loginForm;
---
+++ src/app/(aplicacao)/login/page.jsx
@@ -0,0 +1,11 @@
+import LoginForm from '../../../Components/LoginForm/index'
+
+const Page = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Page;
